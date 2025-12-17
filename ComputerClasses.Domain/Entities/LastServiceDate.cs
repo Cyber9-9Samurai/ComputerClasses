2d253a70bd28330507edba7c93738cf9c57927c1
Artem <75899192+Chel0vek690@users.noreply.github.com>
2025-12-17 14:51:02 +0400

    feat(DAL | Domain): The logic for importing data from Excel has been implemented.

---
+++ ComputerClasses.Domain/Entities/LastServiceDate.cs
@@ -0,0 +1,8 @@
+﻿namespace ComputerClasses.Domain.Entities
+{
+    public class LastServiceDate
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ ComputerClasses.Domain/RowsName.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComputerClasses.Domain
+{
+    public static class RowsName
+    {
+        public const string name  = "Идентификатор";
+        public const string departmentOrInstitute = "Кафедра/Институт";
+        public const string frame = "Корпус";
+        public const string audienceNumber = "Номер аудитории";
+        public const string audienceName = "Название аудитории";
+        public const string responsiblePerson = "Ответственный";
+        public const string inventoryNumber = "Инвентарный номер";
+        public const string operatingSystem = "ОС";
+        public const string motherboard = "Материнская плата";
+        public const string cpu = "ЦП";
+        public const string videoCard = "Видеокарта";
+        public const string disks = "Диски";
+        public const string ramInGb = "ОЗУ, ГБ";
+        public const string ramType = "Тип ОЗУ";
+        public const string installedApplications = "Установленные приложения";
+        public const string status = "Статус";
+    }
+}
