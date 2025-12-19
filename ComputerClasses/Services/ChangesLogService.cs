3fa6c974ca08f0fd9075568a8fc574ed7de705c4
Philip Shmykov <fshmykov@gmail.com>
2025-12-19 11:54:41 +0400

    +ChangesLogService, +NotificationService

---
+++ ComputerClasses/Services/ChangesLogService.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComputerClasses.Services
+{
+	/// <summary>
+	/// Сервис работы с логами
+	/// </summary>
+	public class ChangesLogService
+	{
+		/// <summary>
+		/// Путь к логам
+		/// </summary>
+		private readonly string _logFilePath;
+
+		public ChangesLogService()
+		{
+			_logFilePath = Path.Combine(
+				AppDomain.CurrentDomain.BaseDirectory, 
+				"changes.log");
+		}
+
+		/// <summary>
+		/// Создать лог
+		/// </summary>
+		/// <param name="message">Текст лога</param>
+		public void Log(string message)
+		{
+			var record = $"{DateTime.Now:yyyy-MM-dd HH:mm:ss} | {message}";
+
+			File.AppendAllText(_logFilePath, record + Environment.NewLine);
+		}
+	}
+}
---
+++ ComputerClasses/Services/NotificationService.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace ComputerClasses.Services
+{
+	/// <summary>
+	/// Сервис уведомлений
+	/// </summary>
+	public class NotificationService
+	{
+		/// <summary>
+		/// Показывает сообщение пользователю
+		/// </summary>
+		/// <param name="message">Текст сообщения</param>
+		public void ShowInfo(string message)
+		{
+			MessageBox.Show(message, "Информация", 
+				MessageBoxButton.OK, MessageBoxImage.Information);
+		}
+
+		/// <summary>
+		/// Показывает сообщение об ошибке пользователю
+		/// </summary>
+		/// <param name="message">Текст сообщения об ошибке</param>
+		public void ShowError(string message)
+		{
+			MessageBox.Show(message, "Ошибка",
+				MessageBoxButton.OK, MessageBoxImage.Error);
+		}
+
+		/// <summary>
+		/// Показывает сообщение подтверждение пользователю
+		/// </summary>
+		/// <param name="message">Текст сообщения подтверждения</param>
+		public bool ShowConfirmation(string message)
+		{
+			var result = MessageBox.Show(message, "Подтверждение",
+				MessageBoxButton.YesNo, MessageBoxImage.Question);
+
+			return result == MessageBoxResult.Yes;
+		}
+	}
+}
