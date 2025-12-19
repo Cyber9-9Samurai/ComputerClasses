1ded07a17fdd16dd23cfd4c4c7583102f51e347e
Альберт Денис <cybersamurai425@gmail.com>
2025-12-19 11:27:37 +0400

    feat(app): Add GetImage service

---
+++ ComputerClasses/BuildExtensions/BuildServicesExtension.cs
@@ -0,0 +1,23 @@
+﻿using ComputerClasses.Services;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComputerClasses.BuildExtensions
+{
+    public static class BuildServicesExtension
+    {
+        public static IHostBuilder BuildServices(this IHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) =>
+            {
+                services.AddScoped<GetLocalImage>();
+            });
+            return builder;
+        }
+    }
+}
---
+++ ComputerClasses/Services/GetLocalImage.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media.Imaging;
+
+namespace ComputerClasses.Services
+{
+    public class GetLocalImage 
+    {
+        private string _imagePath = @"pack://application:,,,/Resources/Assets/Images/";
+
+        public BitmapImage GetImage(string imageName)
+        {
+            return new BitmapImage(new Uri(Path.Combine(_imagePath, imageName)));
+        }
+    }
+}
