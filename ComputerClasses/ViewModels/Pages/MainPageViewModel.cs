af066b16bf62d0202b04f533cfe38b3ecaa5069c
Альберт Денис <cybersamurai425@gmail.com>
2025-12-20 20:40:46 +0400

    style(app): Change output of file data feat(app): Add search on main page

--- ComputerClasses/ViewModels/Pages/MainPageViewModel.cs
+++ ComputerClasses/ViewModels/Pages/MainPageViewModel.cs
@@ -1,7 +1,6 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
 using ComputerClasses.Domain;
-using ComputerClasses.Domain.Import;
 using ComputerClasses.Models;
 using ComputerClasses.Services;
 using ComputerClasses.ViewModels.Abstractions;
@@ -12,19 +11,20 @@ namespace ComputerClasses.ViewModels.Pages
 {
     public partial class MainPageViewModel : PageBaseViewModel
     {
-        [ObservableProperty]
-        private string hi = "hi";
+        
         [ObservableProperty]
         private List<MenuButtonItem> operationButtons = new();
         [ObservableProperty]
         private List<Row> rows = new();
         [ObservableProperty]
-        private MenuButtonItem searchButton;
+        private string searchText;
+
+        private List<Row> baseRows = new();
 
         private readonly Navigator<PopupBaseViewModel> _navigator;
         private readonly GetLocalImage _imageService;
         private readonly WorkFileService _workFileService;
-        
+
         public MainPageViewModel(Navigator<PopupBaseViewModel> navigator, GetLocalImage imageService, WorkFileService workFileService)
         {
             _navigator = navigator;
@@ -35,27 +35,36 @@ namespace ComputerClasses.ViewModels.Pages
 
         private void LoadData()
         {
-            SearchButton = new MenuButtonItem("Найти", _imageService.GetImage("Loading.gif"), DoSearchCommand);
             OperationButtons = new List<MenuButtonItem>()
             {
                 new MenuButtonItem("Добавить",_imageService.GetImage("Add.gif"),AddCommand),
                 new MenuButtonItem("Редактировать",_imageService.GetImage("Edit.gif"),EditCommand),
                 new MenuButtonItem("Удалить",_imageService.GetImage("Delete.gif"),RemoveCommand)
             };
-            _workFileService.PropertyChanged += (s, e) =>{ 
+            _workFileService.PropertyChanged += (s, e) =>
+            {
                 if (_workFileService.fileChanged == e.PropertyName)
-                { 
-                    LoadFile(); 
-                } 
-            }; 
+                {
+                    LoadFile();
+                }
+            };
+
+            this.PropertyChanged += (s, e) =>
+            {
+                if (e.PropertyName == nameof(SearchText))
+                {
+                    DoSearch();
+                }
+            };
         }
 
         private void LoadFile()
         {
+            baseRows = _workFileService.ImportData.Items;
             Rows = _workFileService.ImportData.Items;
         }
 
-        
+
 
         [RelayCommand]
         private void OpenPopup()
@@ -63,8 +72,19 @@ namespace ComputerClasses.ViewModels.Pages
             _navigator.Navigate<TestPopupViewModel>();
         }
 
-        [RelayCommand]
-        private void DoSearch() { }
+
+        private void DoSearch()
+        {
+            var text = SearchText.ToLower();
+            if (!string.IsNullOrWhiteSpace(text))
+            {
+                Rows = baseRows.Where(i => i.AudienceName.Name.ToLower().Contains(text) || i.ApplicationList.Name.ToLower().Contains(text)).ToList();
+            }
+            else if(baseRows.Count > Rows.Count)
+            {
+                Rows = baseRows;
+            }
+        }
 
         [RelayCommand]
         private void Add()
