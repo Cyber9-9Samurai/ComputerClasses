3421f609c8b58a1d026f16bfea3c44ca5b915373
Альберт Денис <cybersamurai425@gmail.com>
2025-12-20 20:41:48 +0400

    feat(app): Add export xaml markup and pattern of export logic

--- ComputerClasses/ViewModels/Pages/ExportPageViewModel.cs
+++ ComputerClasses/ViewModels/Pages/ExportPageViewModel.cs
@@ -1,14 +1,62 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using ComputerClasses.Models;
 using ComputerClasses.ViewModels.Abstractions;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ComputerClasses.ViewModels.Pages
 {
     public partial class ExportPageViewModel : PageBaseViewModel
     {
+        [ObservableProperty]
+        private List<string> exportVar = new();
+        [ObservableProperty]
+        private string selectedExportVar;
+
+        public ExportPageViewModel()
+        {
+            LoadData();
+            SelectedExportVar = ExportVar.FirstOrDefault() ?? string.Empty;
+        }
+
+        private void LoadData()
+        {
+            var variants = typeof(ExportVariants).GetFields();
+            foreach (var field in variants)
+            {
+                if (field.GetValue(null) is object value && value is not null)
+                {
+                    ExportVar.Add(value.ToString()!);
+                }
+            }
+        }
+
+        [RelayCommand]
+        private void Export()
+        {
+            switch (SelectedExportVar)
+            {
+                case ExportVariants.ThisFile:
+                    {
+
+                        break;
+                    }
+                case ExportVariants.ExelXLS:
+                    {
+                        break;
+                    }
+                case ExportVariants.ExelXLSX:
+                    {
+                        break;
+                    }
+                case ExportVariants.Csv:
+                    {
+                        break;
+                    }
+                case ExportVariants.PDF:
+                    {
+                        break;
+                    }
+            }
+        }
+       }
     }
-}
