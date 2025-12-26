a57ca2d9927a0e9c9f030fe62f6263ab31f9fd89
Альберт Денис <cybersamurai425@gmail.com>
2025-12-26 04:13:10 +0400

    feat(app): add errors and success popups instead of messageboxes

---
+++ ComputerClasses/ViewModels/Popups/ErrorPopupViewModel.cs
@@ -0,0 +1,38 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using ComputerClasses.ViewModels.Abstractions;
+using Mvvm.Navigation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComputerClasses.ViewModels.Popups
+{
+    public partial class ErrorPopupViewModel : PopupBaseViewModel
+    {
+        private readonly Navigator<PopupBaseViewModel> _navigator;
+        [ObservableProperty]
+        private string errorMessage = "Ошибка.Что - то пошло не так!";
+        public ErrorPopupViewModel(Navigator<PopupBaseViewModel> navigator)
+        {
+            _navigator = navigator;
+        }
+
+        public void SetDescription(string errorMessage)
+        {
+            if (!string.IsNullOrEmpty(errorMessage))
+            {
+                ErrorMessage += "\n" + errorMessage;
+            }
+        }
+
+        [RelayCommand]
+        private void Close()
+        {
+            _navigator.Navigate<EmptyPopupViewModel>();
+            _navigator.BackStack.Clear();
+        }
+    }
+}
---
+++ ComputerClasses/ViewModels/Popups/SuccessPopupViewModel.cs
@@ -0,0 +1,30 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using ComputerClasses.ViewModels.Abstractions;
+using Mvvm.Navigation;
+
+namespace ComputerClasses.ViewModels.Popups
+{
+    public partial class SuccessPopupViewModel : PopupBaseViewModel
+    {
+        private readonly Navigator<PopupBaseViewModel> _navigatorPopup;
+        [ObservableProperty]
+        private string description = "Операция прошла успешно!";
+        public SuccessPopupViewModel(Navigator<PopupBaseViewModel> navigator)
+        {
+            _navigatorPopup = navigator;
+        }
+
+        public void SetDescription(string description)
+        {
+            Description += "\n" + description;
+        }
+
+        [RelayCommand]
+        private void Close()
+        {
+            _navigatorPopup.Navigate<EmptyPopupViewModel>();
+            _navigatorPopup.BackStack.Clear();
+        }
+    }
+}
